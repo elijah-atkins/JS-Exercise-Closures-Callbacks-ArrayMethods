4d66f81bcf282420c2488abe7b984a688e48e034
elijah-atkins <elijahorama@gmail.com>
2020-02-12 20:52:41 -0700

    3 passing

--- index.js
+++ index.js
@@ -68,8 +68,10 @@ function processLength(list, callback) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
 */
-function processLastItem(/* CODE HERE */) {
+function processLastItem(stringList, callback) {
   /* CODE HERE */
+  let lastString = stringList[stringList.length -1];
+  return callback(lastString);
 }
 
 /**
